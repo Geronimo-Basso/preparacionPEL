2d7f930d960c2a4aee334ee78f6f02694ef80030
Geronimo Basso <gerobasso01@gmail.com>
2023-01-21 17:07:46 +0100

    segundo

---
+++ CommandsUF2.h
@@ -0,0 +1,15 @@
+//
+// Created by Geronimo Basso on 21/1/23.
+//
+
+#ifndef PREPARACION_COMMANDSUF2_H
+#define PREPARACION_COMMANDSUF2_H
+class CommandsUF2{
+    sizeof(); //: Permite conocer el tamaño en bytes de los tipos de datos.
+    alignof(); //: Muestra los datos de alineación en memoria.
+    alignas(); //: Permite modificar la alineación natural de una variable siempre y cuando no debilite la alineación original de la misma
+    delete(); //Llama al destructor
+    // Forma de declarar en el free store int * hola = new int(7)
+};
+
+#endif //PREPARACION_COMMANDSUF2_H
